a818cb55bf5579951871710a4d2a04bab1462e65
ring2bell <ohesuk123@example.com>
2022-10-04 15:12:16 +0900

    망고샵 노드서버 생성

---
+++ index.js
@@ -0,0 +1,22 @@
+var http = require("http");
+var hostname = '127.0.0.1'
+var port = '8080'
+
+const server = http.createServer(function (req, res) {
+    const path = req.url
+    const method = req.method
+    if (path === '/products') {
+        if (method === 'GET') {
+            res.writeHead(200, { "Content-Type": "application/json" });
+            const products = JSON.stringify([{ name: "배변패드", price: 50000 }]);
+            res.end(products);
+        } else if (method === 'POST') {
+            res.end("👟 POST success");
+        }
+        res.end('GOOD BOY')
+    }
+    res.end('GOOD BOY')
+})
+
+server.listen(port, hostname)
+console.log('mangoshop server on')
